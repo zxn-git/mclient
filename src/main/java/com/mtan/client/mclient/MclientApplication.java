85c9eec9798aa5f77b6492834cc17aa39616c727
zxn-git <461454959@qq.com>
2019-03-28 12:48:49 +0800

    first commit

---
+++ src/main/java/com/mtan/client/mclient/MclientApplication.java
@@ -0,0 +1,13 @@
+package com.mtan.client.mclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MclientApplication.class, args);
+    }
+
+}
